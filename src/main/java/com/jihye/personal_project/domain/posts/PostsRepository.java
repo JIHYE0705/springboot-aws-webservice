3a9d728136b02faab160a998c1995a57db322f58
jihye0705 <jihyem75@gmail.com>
2022-07-08 21:06:59 +0900

    feat : Posts Repository

---
+++ src/main/java/com/jihye/personal_project/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.jihye.personal_project.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
