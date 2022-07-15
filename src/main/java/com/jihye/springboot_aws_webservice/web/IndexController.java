05b28387788915ebd5b4a9cb4668e79d35065b6e
jihye0705 <jihyem75@gmail.com>
2022-07-15 23:14:44 +0900

    feat: login

--- src/main/java/com/jihye/springboot_aws_webservice/web/IndexController.java
+++ src/main/java/com/jihye/springboot_aws_webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jihye.springboot_aws_webservice.web;
 
+import com.jihye.springboot_aws_webservice.config.auth.dto.SessionUser;
 import com.jihye.springboot_aws_webservice.service.posts.PostsService;
 import com.jihye.springboot_aws_webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(Objects.nonNull(user)) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
